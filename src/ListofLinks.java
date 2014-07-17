d4e8dac692ac441bb0b097cc607c8ddbf35dc058
fly <fly82@ukr.net>
2014-07-17 22:09:02 +0300

    ok

---
+++ src/ListofLinks.java
@@ -0,0 +1,35 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListofLinks {
+    public static void main(String... args) throws IOException {
+        List<String> ls = new ArrayList<>();
+        FileReader fr = null;
+        try {
+            fr = new FileReader(args[0]);
+            int b;
+            boolean sw = true;
+            String str = "";
+
+            while ((b = fr.read()) != -1) {
+                if (b > 47 && b < 59) {
+                    sw = true;
+                    str += ((char) b);
+                }
+                if ((b < 48 || b > 58) && sw) {
+                    sw = false;
+                    str = args[1] + str + args[2];
+                    ls.add(str);
+                    str = "";
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            if (fr != null) fr.close();
+        }
+        ls.stream().forEach(System.out::println);
+    }
+}
